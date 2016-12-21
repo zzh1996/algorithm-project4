5ccce05736c7982e369290f8cd50b6ce3f0c5ab6
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-12-21 15:28:42 +0800

    timer for scc

--- project1/Source/scc.c
+++ project1/Source/scc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <time.h>
 
 #define NIL -1
 
@@ -25,12 +26,12 @@ int ordered_vertex_p;
 int sccs[729*2];
 int sccs_p;
 
-int time;
+int time_;
 
 void dfs_visit(int i,int n){
 	struct edge *p;
-	time++;
-	v[i].d=time;
+	time_++;
+	v[i].d=time_;
 	v[i].color=GRAY;
 	for(p=v[i].adj;p;p=p->next){
 		if(v[p->v].color==WHITE){
@@ -39,8 +40,8 @@ void dfs_visit(int i,int n){
 		}
 	}
 	v[i].color=BLACK;
-	time++;
-	v[i].f=time;
+	time_++;
+	v[i].f=time_;
 	ordered_vertex[ordered_vertex_p]=i;
 	ordered_vertex_p--;
 }
@@ -51,7 +52,7 @@ void dfs(int n){
 		v[i].color=WHITE;
 		v[i].pi=NIL;
 	}
-	time=0;
+	time_=0;
 	ordered_vertex_p=n-1;
 	for(i=0;i<n;i++){
 		if(v[i].color==WHITE){
@@ -63,8 +64,8 @@ void dfs(int n){
 void dfs_visit2(int i,int n){
 	struct edge *p;
 	sccs[sccs_p++]=i;
-	time++;
-	vt[i].d=time;
+	time_++;
+	vt[i].d=time_;
 	vt[i].color=GRAY;
 	for(p=vt[i].adj;p;p=p->next){
 		if(vt[p->v].color==WHITE){
@@ -73,8 +74,8 @@ void dfs_visit2(int i,int n){
 		}
 	}
 	vt[i].color=BLACK;
-	time++;
-	vt[i].f=time;
+	time_++;
+	vt[i].f=time_;
 }
 
 void dfs2(int n){
@@ -83,7 +84,7 @@ void dfs2(int n){
 		vt[i].color=WHITE;
 		vt[i].pi=NIL;
 	}
-	time=0;
+	time_=0;
 	sccs_p=0;
 	for(i=0;i<n;i++){
 		if(vt[ordered_vertex[i]].color==WHITE){
@@ -112,14 +113,16 @@ int main(){
 	int i,j,n=3,a,b;
 	FILE *fp;
 	char filename[100];
+	double diff;
+    struct timespec start,stop;
 	for(i=0;i<5;i++){
 		n*=3;
 		for(j=0;j<n;j++){
-			v[i].d=vt[i].d=-1;
-			v[i].pi=vt[i].pi=-1;
-			v[i].color=vt[i].color=-1;
-			v[i].f=vt[i].f=-1;
-			v[i].adj=vt[i].adj=NULL;
+			v[j].d=vt[j].d=-1;
+			v[j].pi=vt[j].pi=-1;
+			v[j].color=vt[j].color=-1;
+			v[j].f=vt[j].f=-1;
+			v[j].adj=vt[j].adj=NULL;
 		}
 		sprintf(filename,"../Input/size%d/input.txt",i+1);
 		fp=fopen(filename,"r");
@@ -131,21 +134,30 @@ int main(){
 		}
 		fclose(fp);
 
+		clock_gettime(CLOCK_MONOTONIC,&start); //开始计时
 		scc(n);
+		clock_gettime(CLOCK_MONOTONIC,&stop); //结束计时
+		diff=(stop.tv_sec-start.tv_sec)+(double)(stop.tv_nsec-start.tv_nsec)/1000000000L;
+		sprintf(filename,"../Output/size%d/time1.txt",i+1);
+		fp=fopen(filename,"w");
+        fprintf(fp,"%.9fs\n",diff);
+        fclose(fp);
 
-		printf("(");
+        sprintf(filename,"../Output/size%d/output1.txt",i+1);
+        fp=fopen(filename,"w");
+		fprintf(fp,"(");
 		for(j=0;j<sccs_p-1;j++){
 			if(sccs[j]<0){
-				printf(")\n(");
+				fprintf(fp,")\n(");
 			}else{
-				printf("%d",sccs[j]);
+				fprintf(fp,"%d",sccs[j]);
 				if(sccs[j+1]>=0){
-					printf(",");
+					fprintf(fp,",");
 				}
 			}
 		}
-		printf(")\n\n");
-		
+		fprintf(fp,")\n\n");
+		fclose(fp);
 	}
 	return 0;
 }
